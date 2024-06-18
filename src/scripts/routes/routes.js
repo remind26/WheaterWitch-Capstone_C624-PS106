2dd98902241cc0b39adc39209f5d6cfe6837c67e
Muhammad Ridhwan Ramadhan <98072400+remind26@users.noreply.github.com>
2024-06-19 01:44:22 +0700

    Update routes.js

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -5,13 +5,9 @@ import Recipes from '../views/pages/resep-masak';
 import MoviesPage from '../views/pages/menonton-film';
 import Book from '../views/pages/membaca-buku';
 import OutdoorActivity from '../views/pages/outdoor-activity';
-import Login from '../views/pages/login';
-import Logout from '../views/pages/logout';
-import Register from '../views/pages/Register';
 
 const routes = {
-  '/': Login,
-  '/login': Login,
+  '/': Home,
   '/home': Home,
   '/weather': WeatherWitch,
   '/aboutus': Aboutus,
@@ -19,8 +15,6 @@ const routes = {
   '/menonton-film': MoviesPage,
   '/membaca-buku': Book,
   '/outdoor-activity': OutdoorActivity,
-  '/logout': Logout,
-  '/register': Register,
 };
 
 export default routes;
